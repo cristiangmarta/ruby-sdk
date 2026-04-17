9aabd8de36393061a9729f7951455e7817fdb9c2
Cristian Marta <cristian@caffe-lento.com>
2026-04-17 02:47:29 -0300

    fix: avoid duplicate targeting_key kwarg in EvaluationContext#merge
    Signed-off-by: Cristian Marta <cristian@caffe-lento.com>

--- lib/open_feature/sdk/evaluation_context.rb
+++ lib/open_feature/sdk/evaluation_context.rb
@@ -20,10 +20,10 @@ module OpenFeature
       end
 
       def merge(overriding_context)
-        EvaluationContext.new(
-          targeting_key: overriding_context.targeting_key || targeting_key,
-          **fields.merge(overriding_context.fields)
-        )
+        merged = fields
+          .merge(overriding_context.fields)
+          .merge(TARGETING_KEY => overriding_context.targeting_key || targeting_key)
+        EvaluationContext.new(**merged.transform_keys(&:to_sym))
       end
 
       def ==(other)
